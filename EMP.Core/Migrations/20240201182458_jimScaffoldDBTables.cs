6b2fb04a85d14f9b1a4819bf3c535df6a3d4e9b7
Jim Clifford <jim_clifford@hotmail.com>
2024-02-01 13:28:00 -0500

    Initial DB Migration

---
+++ EMP.Core/Migrations/20240201182458_jimScaffoldDBTables.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EMP.Core.Migrations
+{
+    public partial class jimScaffoldDBTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
